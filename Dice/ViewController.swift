3c98008223a5fdbca3024b5ced34507749baf2a4
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2023-12-07 14:23:11 +0100

    Dice App

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -4,16 +4,24 @@
 //
 //  Created by Mehtap Parkinson on 4/12/23.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        diceImageViewOne.image = diceArray.randomElement()
+        leftDiceNumber += 1
+        diceImageViewTwo.image  = diceArray.randomElement()
+        rightDiceNumber -= 1
+      
+        
     }
-
-
+    
+    
 }
-
